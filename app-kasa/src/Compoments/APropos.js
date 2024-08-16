d0f95cc333fedd45bd0533e268d3e1274f965c45
Etienne Colmant <colmant.etienne@gmail.com>
2024-08-16 10:11:37 +0200

    preparation page acceuil et a propos

--- app-kasa/src/Compoments/APropos.js
+++ app-kasa/src/Compoments/APropos.js
@@ -1,35 +1,28 @@
-import '../Styles/App.css';
 import logoHead from '../Assets/.LOGO.png'
 import logoFoot from '../Assets/LOGO.png'
 import CopyRight from '../Assets/© 2020 Kasa. All rights reserved.png'
-import CardsList from './CardsList';
-import APropos from './APropos'
+import App from './App'
+import '../Styles/APropos.css'
 
-
-function App() {
-  return (
-    <div className="App">
+function APropos(){
+    <div className="APropos">
       <header>
         <img src={logoHead} alt='logo kasa' className="kasa-logo-header"/>
         <nav className="kasa-lien-content">
-          <a href to='/' className="kasa-lien">Accueil</a>
-          <a href='/APropos' className="kasa-lien">A Propos</a>
+          <a href='/App' className="kasa-lien">Accueil</a>
+          <a href='/' className="kasa-lien">A Propos</a>
         </nav>
       </header>
       <main>
         <div className='kasa-img-intro'>
-          <div className='kasa-img-intro-text'>
-            <h3>Chez vous, partout et ailleurs</h3>
-          </div>
         </div>
-        <CardsList />
       </main>
       <footer>
       <img src={logoFoot} alt='logo kasa' className="kasa-logo-footer"/>
       <img src={CopyRight} alt='Copyright' className="kasa-lcopyright"/>
       </footer>
     </div>
-  );
+
 }
 
-export default App;
+export default APropos;
--- app-kasa/src/Compoments/Card.js
+++ app-kasa/src/Compoments/Card.js
@@ -4,7 +4,7 @@ import '../Styles/Card.css'
 function Card(){
     return (
         <div className="kasa-card">
-            <p>Titre de la location</p>
+            <p className="kasa-card-title">Titre de la location</p>
         </div>
     )
 
