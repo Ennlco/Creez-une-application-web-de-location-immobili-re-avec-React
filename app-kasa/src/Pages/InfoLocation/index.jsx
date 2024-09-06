0a3e5565b06552d02a5f728e643f58de510e31bc
Etienne Colmant <colmant.etienne@gmail.com>
2024-09-06 14:50:40 +0200

    petite modif

--- app-kasa/src/Pages/InfoLocation/index.jsx
+++ app-kasa/src/Pages/InfoLocation/index.jsx
@@ -1,73 +1,77 @@
-import '../../Styles/Css/infoLocation.css'
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react'
-import {locationList} from '../../Data/LocationList'
-import Carousel from '../../Compoments/Carousel'
-import TitleLoc from '../../Compoments/TitleLoc'
-import Tags from '../../Compoments/Tags'
-import Hote from '../../Compoments/Hote'
-import Rate from '../../Compoments/Rate'
-import Description from '../../Compoments/Description'
-import Equipement from '../../Compoments/Equipement'
+import '../../Styles/Css/infoLocation.css';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { locationList } from '../../Data/LocationList';
+import Carousel from '../../Compoments/Carousel';
+import TitleLoc from '../../Compoments/TitleLoc';
+import Tags from '../../Compoments/Tags';
+import Hote from '../../Compoments/Hote';
+import Rate from '../../Compoments/Rate';
+import Description from '../../Compoments/Description';
+import Equipement from '../../Compoments/Equipement';
 
+function InfoLocation() {
+    const { id } = useParams();
+    const navigate = useNavigate();
 
-
-function InfoLocation(){
-    const {id} = useParams()
     const location = locationList.find(loc => loc.id === id);
 
-    const imgCarousel = location.pictures
-    const titre = location.title
-    const lieu = location.location
-    const tagspage = location.tags
-    const hoteName = location.host.name
-    const hoteImg = location.host.picture
-    const rateStar = location.rating
-    const equip = location.equipments
-    const descrip = location.description
-    
     useEffect(() => {
-		document.title = `Info Location`
-	}, [])
+        if (!location) {
+            navigate('/error'); 
+        }
+    }, [location, navigate]);
 
-    return(
+   
+    useEffect(() => {
+        if (location) {
+            document.title = `Info Location - ${location.title}`;
+        }
+    }, [location]);
+
+
+    if (!location) {
+        return null; 
+    }
+
+   
+    const imgCarousel = location.pictures;
+    const titre = location.title;
+    const lieu = location.location;
+    const tagspage = location.tags;
+    const hoteName = location.host.name;
+    const hoteImg = location.host.picture;
+    const rateStar = location.rating;
+    const equip = location.equipments;
+    const descrip = location.description;
+
+    return (
         <div className='kasa-all-info'>
-            <Carousel 
-            key={imgCarousel}
-            images={imgCarousel}/>
+            <Carousel key={imgCarousel} images={imgCarousel} />
             <div className='kasa-info-base-content'>
                 <div className='kasa-title-tags-content'>
                     <div className='kasa-title-content'>
-                        <TitleLoc 
-                            title={titre}
-                            lieu={lieu}
-                        />
+                        <TitleLoc title={titre} lieu={lieu} />
                     </div>
                     <div className="kasa-tag-content">
-                        <Tags 
-                        tagsElement={tagspage} />
+                        <Tags tagsElement={tagspage} />
                     </div>
                 </div>
                 <div className='kasa-hote-info-content'>
                     <div className='kasa-hote'>
-                        <Hote 
-                            name={hoteName}
-                            profilIng={hoteImg}
-                        />
+                        <Hote name={hoteName} profilIng={hoteImg} />
                     </div>
                     <div className='kasa-rate'>
-                        <Rate 
-                        key={rateStar}
-                        rateValue={rateStar}/>
+                        <Rate key={rateStar} rateValue={rateStar} />
                     </div>
                 </div>
             </div>
             <div className='kasa-info-plus'>
-                <Description descrip={descrip}/>
-                <Equipement equip={equip}/>
+                <Description descrip={descrip} />
+                <Equipement equip={equip} />
             </div>
         </div>
-    )
+    );
 }
 
 export default InfoLocation;
